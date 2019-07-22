5853e80944c91106ce806143eea265df5d4691ed
Estelle K <e.keaton@live.com>
2019-07-22 13:33:04 -0700

    removed fixed position from demo nav

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -12,7 +12,7 @@ function HomePage() {
         <h3>Why DreamHome?</h3>
        <p>DreamHome is a virtual home vision board at your fingertips. Each board is conveniently separated, allowing you to keep your images organized per room. 
        Found the perfect flooring for your kitchen? Save it to your kitchen board, it's that easy. 
-       Ideal for anyone who is building their own home, remodeling or just wants to create a vision board of what their ideal dream home would look like.</p>
+       Ideal for anyone who is building their own home, remodeling or just want to create a vision board of what their ideal dream home would look like.</p>
      </section>
      </>
     )
