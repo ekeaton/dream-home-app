1abeaf17b016b5b5178f2c66d1e78a4e5b3e45d1
Estelle K <e.keaton@live.com>
2019-07-29 13:56:09 -0700

    removed bug

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -15,12 +15,12 @@ function HomePage() {
             <h2>Why DreamHome?</h2>
         </header>
         <div className="row">
-        <div className=".col-6 w3-center">
+        <div className="col-6">
           <img src="https://i.imgur.com/qwl6oeG.png"  alt="floorplan-icon" className="icon-1"/>
           <h4>Floorplans</h4>
              <p>The first step in building your dream home starts with the floorplan. Here at DreamHome we've done the leg work for you and found the most popular home plans.</p>
         </div>
-        <div className=".col-6 w3-center">
+        <div className="col-6">
           <img src="https://i.imgur.com/it9vrby.png" alt='notes-icon' className="icon"/>
           <h4>Notes</h4>
          <p>Think you've found the perfect one or can't decide between your two favorites. Add a note and refer back to them at any time.</p>
